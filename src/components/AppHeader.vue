b99b3d76dcb0e31536e8329e514d9ac8142c184c
chadleyderose <chadleyderose1911201@gmail.com>
2021-09-09 16:13:11 +0200

    Commit-2-9/sep

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,30 @@
 <template>
-  
+  <div>
+      <nav class="navbar navbar-expand navbar-dark bg-dark"> 
+          <div class="container">
+            <div class="nav navbar-nav">
+                <a class="nav-item nave-link active" href="#">Home</a>
+                <a class="nav-item nave-link" href="#">product</a>
+            </div>
+            <div>
+                <div class="dropdown open">
+                    <button 
+                        class="btn btn-secondary dropdown-toggle" 
+                        type="button" 
+                        id="triggerId" 
+                        data-toggle="dropdown" 
+                        aria-haspopup="true" 
+                        aria-expanded="false"
+                    >Dropdown</button>
+                    <div class="dropdown-menu" aria-labelledby="triggerId">
+                        <button class="dropdown-item" href="#">Action</button>
+                        <button class="dropdown-item disabled" href="#">Disabled action</button>
+                    </div>
+                </div>
+            </div>
+            </div>
+      </nav>
+  </div>
 </template>
 
 <script>
