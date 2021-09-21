f785aa0950e470aa9148aebef5e622e6ad58ddc7
chadleyderose <chadleyderose1911201@gmail.com>
2021-09-21 16:16:26 +0200

    commit8-21/sep

--- src/apis/Cart.js
+++ src/apis/Cart.js
@@ -1,10 +1,20 @@
 import Api from "./Api";
 
+const END_POINT = 'cart'
+
 export default{
     all(){
-        return Api.get('cart')
+        return Api.get(END_POINT)
     },
+
     store(data){
-        return Api.post('cart', data)
+        return Api.post(END_POINT, data)
+    },
+
+    delete(id){
+        return Api.delete(`${END_POINT}/${id}`)
+    },
+    deleteAll(){
+        return Api.delete(END_POINT)
     }
 }
--- src/apis/Products.js
+++ src/apis/Products.js
@@ -1,12 +1,14 @@
 import Api from "./Api";
 
+const END_POINT = product
+
 export default{
     all (){
-        return Api.get('products');
+        return Api.get(END_POINT);
     },
 
     show(id){
-       return Api.get(`products/${id}`);
+       return Api.get(`${END_POINT}/${id}`);
         
     }
 }
